8f5163e07484bd8cce0191f2eed3c7625974df19
David <dmaine6711@gmail.com>
2023-11-01 15:12:15 -0400

    deep dup

--- practice-for-ch-ruby-recursion-long-practice-main/test.rb
+++ practice-for-ch-ruby-recursion-long-practice-main/test.rb
@@ -14,7 +14,30 @@ def exp(base, pow)
 end
 
 
+def exp2(base, pow)
+    return 1 if pow == 0 #good
+    return 0 if base == 0 #good
+
+    if pow.even? 
+      return exp2(base, pow / 2) ** 2
+    elsif pow.odd?
+        return base * (exp2(base, (pow - 1) / 2) ** 2)
+    end         
+end    
+
+class Array
+
+    def deep_dup  
+        new_arr = []
+    self.each do |el|
+        if el.is_a?(Array)
+            new_arr << el.deep_dup      
+        else 
+            new_arr << el
+        end
+    end
+    new_arr
+    end    
+end
 
 
-
-#hi
